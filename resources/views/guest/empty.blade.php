94d1d193fef3dd1c0027cc88fe9bc454579f2670
Michela-DC <micheladelconte@outlook.com>
2022-05-26 22:10:05 +0200

    Updated layout-style of empty.blade

--- resources/views/guest/empty.blade.php
+++ resources/views/guest/empty.blade.php
@@ -22,27 +22,29 @@
 
 <body>
 
-    <header class="py-5">
-        <div class="container">
-            <div class="login-container text-end mb-3 mx-3">
+    <main class="main-container">
+        <div class="main-row d-flex flex-column align-items-center justify-content-center h-100 w-100">
+            <h2 class="mb-4">You still have to upload your horoscopes!</h2>
+    
+            <h4 class="mb-4">Login/register to upload:</h4>
+    
+            <div class="login-container">
                 @if (Route::has('login'))
                 <div>
                     @auth
-                    <a class="text-decoration-none" href="{{ url('/upload') }}">Go to upload</a>
+                        <a class="text-decoration-none" href="{{ url('/upload') }}">Go to upload</a>
                     @else
-                    <a class="text-decoration-none mx-3" href="{{ route('login') }}">Login</a>
+                        <a class="text-decoration-none mx-3" href="{{ route('login') }}">Login</a>
                     
                     @if (Route::has('register'))
-                    <a class="text-decoration-none" href="{{ route('register') }}">Register</a>
+                        <a class="text-decoration-none" href="{{ route('register') }}">Register</a>
                     @endif
                     @endauth
                 </div>
                 @endif
             </div>
-            
-            NOOOOO
         </div>
-    </header>
+    </main>
     
     <script src="{{asset('js/front.js')}}"></script>
 </body>
